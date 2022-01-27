2459c869e2f01847b74b51a18a284994af2cfa96
Henry Lai <e0032134@u.nus.edu>
2022-01-27 08:57:47 +0000

    flow and for js file

---
+++ 04-loops/flow-control.js
@@ -0,0 +1,22 @@
+
+// break
+let n = 0;
+while(n<10){
+    if(n >=5){
+        break;
+    }
+    n++;
+    console.log("line "+n);
+}
+
+//continue
+
+for (let s = 10; s<20; s++)
+{
+    if(s == 12)
+    {
+        continue;
+    }
+
+    console.log(s);
+}
